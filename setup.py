51bc4c3363db27038d3ae3d4224c128e9c948a80
Will <author@will.party>
2020-02-01 02:46:31 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='terminalle',
+      version='1.0',
+      author='Will Noble',
+      author_email='code@will.party',
+      description='A fancy drop-down terminal emulateur',
+      url='https://github.com/wm-noble/terminalle',
+      packages=['terminalle'],
+      python_requires='>=3.6')
