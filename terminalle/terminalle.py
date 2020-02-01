4ffdd5367180885c46baac9d70525c01d2cfac8d
Will <author@will.party>
2020-02-01 14:02:59 -0800

    Clean up terminalle.py

--- terminalle/terminalle.py
+++ terminalle/terminalle.py
@@ -7,77 +7,63 @@ gi.require_version('Vte', '2.91')
 from gi.repository import Gtk, Gdk, GLib, GObject, Vte
 
 class Terminalle:
+    """ Manages the window. """
 
-    def __init__(self, settings: Dict, show: bool):
-        self.show_on_startup = show
-
-        self.wnd = Gtk.Window()
+    def __init__(self, settings: Dict[str, object], show: bool):
+        """ Initialize the window and VTE widget. """
+        wnd = Gtk.Window()
+        wnd.set_title('Terminalle')
+        wnd.fullscreen()
+        wnd.set_keep_above(True)
+        wnd.set_skip_taskbar_hint(True)
+        wnd.set_skip_pager_hint(True)
+        wnd.set_type_hint(Gdk.WindowTypeHint.DOCK)
         # For some reason the top-level window opacity must not be 1
         # in order to enable any kind of child widget transparency.
-        self.wnd.set_opacity(.99)
-        self.wnd.set_title('Terminalle')
-        self.wnd.set_hide_titlebar_when_maximized(True)
-        self.wnd.maximize()
-        self.wnd.fullscreen()
-        self.wnd.set_keep_above(True)
-        self.wnd.set_skip_taskbar_hint(True)
-        self.wnd.set_skip_pager_hint(True)
-        self.wnd.set_type_hint(Gdk.WindowTypeHint.DOCK)
-        self.wnd.set_events(Gdk.EventType.FOCUS_CHANGE)
-        self.wnd.connect('focus-out-event', self._focus_out)
+        wnd.set_opacity(.99)
+        wnd.set_events(Gdk.EventType.FOCUS_CHANGE)
+        wnd.connect('focus-out-event', self._focus_out)
 
-        self.term = Vte.Terminal()
-        self.term.connect('child-exited', self._term_exited)
-        self.term.set_font(font_desc=settings['font'])
-        self.term.set_allow_bold(True)
-        self.term.set_allow_hyperlink(True)
+        term = Vte.Terminal()
+        term.set_font(font_desc=settings['font'])
+        term.set_allow_bold(True)
+        term.set_allow_hyperlink(True)
         bg = settings['colors'][0].copy()
         bg.alpha = settings['opacity']
-        self.term.set_colors(background=bg, palette=settings['colors'])
+        term.set_colors(background=bg, palette=settings['colors'])
+        term.connect('child-exited', self._term_exited)
+        wnd.add(term)
 
-        success, pid = self.term.spawn_sync(
+        # Ctrl+Shift+(C|V) to use the clipboard.
+        accel_group = Gtk.AccelGroup()
+        _init_ctrl_shift_handler(wnd, accel_group, 'C', self._copy_clipboard)
+        _init_ctrl_shift_handler(wnd, accel_group, 'V', self._paste_clipboard)
+        wnd.add_accel_group(accel_group)
+
+        self.wnd = wnd
+        self.term = term
+        self.settings = settings
+        self.show_on_startup = show
+
+    def run(self):
+        """ Open the TTY and enter the GTK main loop. """
+        success, vte_pid = self.term.spawn_sync(
             pty_flags=Vte.PtyFlags.DEFAULT,
-            working_directory=settings['home'],
-            argv=[settings['shell']],
+            working_directory=self.settings['home'],
+            argv=[self.settings['shell']],
             envv=None,
             spawn_flags=GLib.SpawnFlags.DO_NOT_REAP_CHILD)
         if not success:
-            del self.term
             self.wnd.close()
-            del self.wnd
-            raise RuntimeError('error spawning VTE terminal')
-        self.wnd.add(self.term)
-
-        self.accel_group = Gtk.AccelGroup()
-        self._init_ctrl_shift_handler('C', self._copy_clipboard)
-        self._init_ctrl_shift_handler('V', self._paste_clipboard)
-        self.wnd.add_accel_group(self.accel_group)
-
-    def _init_ctrl_shift_handler(self, key_name: str, handler: Callable):
-        signal_name = 'ctrl-shift-' + key_name
-        GObject.signal_new(signal_name, Gtk.Window,
-                           GObject.SignalFlags.RUN_LAST | GObject.SignalFlags.ACTION,
-                           None, ())
-        self.wnd.connect(signal_name, handler)
-        self.wnd.add_accelerator(signal_name, self.accel_group,
-                                 Gdk.keyval_from_name(key_name),
-                                 Gdk.ModifierType.CONTROL_MASK | Gdk.ModifierType.SHIFT_MASK,
-                                 Gtk.AccelFlags.LOCKED)
-
-    def run(self):
+            raise RuntimeError('Error spawning VTE terminal.')
         self.wnd.show_all()
         if not self.show_on_startup:
             self.wnd.hide()
         Gtk.main()
 
     def toggle(self):
+        """ Toggle window visibility. """
         GLib.idle_add(self._toggle)
-
-    def _copy_clipboard(self, wnd: Gtk.Window):
-        self.term.copy_clipboard_format(Vte.Format.TEXT)
-
-    def _paste_clipboard(self, wnd: Gtk.Window):
-        self.term.paste_clipboard()
 
     def _toggle(self):
         if self.wnd.is_active():
@@ -86,15 +72,36 @@ class Terminalle:
             self.wnd.show()
             self.wnd.grab_focus()
 
-    def _focus_out(self, wnd: Gtk.Window, event: Gdk.EventFocus):
-        self.wnd.hide()
+    def _copy_clipboard(self, wnd: Gtk.Window):
+        self.term.copy_clipboard_format(Vte.Format.TEXT)
 
-    def quit(self):
-        GLib.idle_add(Gtk.main_quit)
+    def _paste_clipboard(self, wnd: Gtk.Window):
+        self.term.paste_clipboard()
+
+    def _focus_out(self, wnd: Gtk.Window, event: Gdk.EventFocus):
+        GLib.idle_add(self.wnd.hide)
 
     def _term_exited(self, term: Vte.Terminal, status: int):
-        self.wnd.remove(self.term)
-        del self.term
+        self.quit()
+
+    def quit(self):
+        """ Close the window and exit the GTK main loop. """
+        GLib.idle_add(self._quit)
+
+    def _quit(self):
         self.wnd.close()
-        del self.wnd
-        Gtk.main_quit()
+        GLib.idle_add(Gtk.main_quit)
+
+def _init_ctrl_shift_handler(wnd: Gtk.Window,
+                             accel_group: Gtk.AccelGroup,
+                             key_name: str,
+                             handler: Callable):
+    signal_name = 'ctrl-shift-' + key_name
+    GObject.signal_new(signal_name, Gtk.Window,
+                       GObject.SignalFlags.RUN_LAST | GObject.SignalFlags.ACTION,
+                       None, ())
+    wnd.connect(signal_name, handler)
+    wnd.add_accelerator(signal_name, accel_group,
+                        Gdk.keyval_from_name(key_name),
+                        Gdk.ModifierType.CONTROL_MASK | Gdk.ModifierType.SHIFT_MASK,
+                        Gtk.AccelFlags.LOCKED)
