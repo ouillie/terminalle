39edfb63df7fe4427ca9939e57e833e9944b7782
Will <author@will.party>
2021-01-19 19:19:12 -0800

    Add command-line tools to facilitate autostart

--- terminalle/__main__.py
+++ terminalle/__main__.py
@@ -1,6 +1,85 @@
 #!/usr/bin/python3
 
-from . import main
+from os import getenv
+from os.path import join as join_path
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from dbus import SessionBus
+from dbus.service import Object as DBusObject, BusName, method
+from dbus.mainloop.glib import DBusGMainLoop
+
+from . import Terminalle, load_settings, service_name
+from .auto import auto, no_auto
+
+_home_path = getenv('HOME', '~')
+
+class Controller(DBusObject):
+
+    def __init__(self, term, bus, object_path='/termctl'):
+        super().__init__(bus, object_path)
+        self.term = term
+
+    @method(service_name)
+    def Toggle(self):
+        self.term.toggle()
+
+    @method(service_name)
+    def Quit(self):
+        self.term.quit()
+
+def build_parser():
+    parser = ArgumentParser(description='A fancy drop-down terminal emulateur.',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--config', metavar='PATH',
+                        help='load config settings from PATH',
+                        default=join_path(_home_path, '.config', 'terminalle.yaml'))
+    parser.add_argument('-s', '--show', action='store_true',
+                        help='show the window immediately on startup')
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand',
+                                       description='''
+            Use `<subcommand> --help` for more information.''')
+    auto_parser = subparsers.add_parser('auto', help='configure auto-start and/or auto-restart',
+                                        description='''
+            Install an XDG desktop file to auto-start Terminalle (in hidden mode) on login,
+            as well as a DBUS service file to auto-restart Terminalle
+            if it\'s not already running when the user attempts to toggle it.''',
+                                        formatter_class=ArgumentDefaultsHelpFormatter)
+    no_auto_parser = subparsers.add_parser('no-auto', help='remove auto-(re)start functionality',
+                                           description='''
+            Remove the DBUS service file and XDG desktop file
+            that were installed using the `auto` subcommand.''',
+                                           formatter_class=ArgumentDefaultsHelpFormatter)
+    for sub_parser in [auto_parser, no_auto_parser]:
+        target_group = sub_parser.add_mutually_exclusive_group()
+        target_group.add_argument('-u', '--user', action='store_true',
+                                  help='apply to the current user only', default=True)
+        target_group.add_argument('-s', '--system', action='store_true',
+                                  help='apply to all users on the system', default=False)
+        sub_parser.add_argument('-f', '--force', action='store_true',
+                                help='delete files even if the contents are unexpected')
+    auto_parser.add_argument('--no-start-on-login', action='store_true',
+                             help='do *not* install XDG desktop file')
+    auto_parser.add_argument('--no-restart-if-closed', action='store_true',
+                             help='do *not* install DBUS service file')
+    return parser
+
+def main():
+    args = build_parser().parse_args()
+    if args.subcommand is None:
+        # By default, run a new instance of the application.
+        loop = DBusGMainLoop(set_as_default=True)
+        bus = SessionBus()
+        name = BusName(service_name, bus)
+        term = Terminalle(settings=load_settings(args.config), show=args.show)
+        ctrl = Controller(term, bus)
+        term.run()
+    elif args.subcommand == 'auto':
+        auto(_home_path, args.system, args.force,
+             not args.no_start_on_login, not args.no_restart_if_closed)
+    elif args.subcommand == 'no-auto':
+        no_auto(_home_path, args.system, args.force)
+    else:
+        raise ValueError(f'Unexpected subcommand \'{args.subcommand}\'.')
 
 if __name__ == '__main__':
     main()
---
+++ terminalle/auto.py
@@ -0,0 +1,81 @@
+""" Command-line tools to install and remove autostart-related files. """
+
+from os import getenv, makedirs, readlink, symlink, unlink
+from os.path import abspath, join as join_path, sep as path_sep, dirname, isfile, islink
+from sys import stderr
+
+from pkg_resources import resource_filename
+
+from . import service_name
+
+def _get_dests_and_srcs(home_path: str, system: bool):
+    service_filename = '.'.join([service_name, 'service'])
+    desktop_filename = 'terminalle.desktop'
+    # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html (for reference)
+    # https://specifications.freedesktop.org/autostart-spec/autostart-spec-latest.html (auto-start)
+    # https://dbus.freedesktop.org/doc/dbus-daemon.1.html (auto-restart)
+    if system:
+        xdg_config_dirs = getenv('XDG_CONFIG_DIRS',
+                                 abspath(join_path(path_sep, 'etc', 'xdg'))).split(':')
+        xdg_data_dirs = getenv('XDG_DATA_DIRS',
+                               abspath(join_path(path_sep, 'usr', 'share'))).split(':')
+    else:
+        xdg_config_dirs = [getenv('XDG_CONFIG_HOME', join_path(home_path, '.config'))]
+        xdg_data_dirs = [getenv('XDG_DATA_HOME', join_path(home_path, '.local', 'share'))]
+    return ([join_path(config_dir, 'autostart', desktop_filename)
+             for config_dir in xdg_config_dirs],
+            resource_filename(__name__, desktop_filename),
+            [join_path(data_dir, 'dbus-1', 'services', service_filename)
+             for data_dir in xdg_data_dirs],
+            resource_filename(__name__, service_filename))
+
+def auto(home_path: str, system: bool, force: bool, start_on_login: bool, restart_if_closed: bool):
+    desktop_dests, desktop_src, service_dests, service_src =  _get_dests_and_srcs(home_path, system)
+    # Pick the least-precendence item in the destination lists.
+    # This tends to be the system-wide rather than 'local' destination.
+    for enable, src, dest in [(start_on_login, desktop_src, desktop_dests[-1]),
+                              (restart_if_closed, service_src, service_dests[-1])]:
+        if enable:
+            if isfile(dest):
+                if islink(dest):
+                    real_src = readlink(dest)
+                    if real_src == src:
+                        print(f'Symlink already exists: {dest}\n'
+                              f'                     -> {src}', file=stderr)
+                        continue
+                    elif force:
+                        unlink(dest)
+                    else:
+                        print(f'Unexpected symlink: {dest}\n'
+                              f'                 -> {real_src}\n'
+                              '(use `--force` to overwrite it)', file=stderr)
+                        continue
+                elif force:
+                    unlink(dest)
+                else:
+                    print(f'Unexpected file: {dest}\n'
+                          '(use `--force` to overwrite it)', file=stderr)
+                    continue
+            else:
+                makedirs(dirname(dest), mode=0o755, exist_ok=True)
+            symlink(src, dest)
+            print(f'Created symlink: {dest}\n'
+                  f'              -> {src}', file=stderr)
+
+def no_auto(home_path: str, system: bool, force: bool):
+    desktop_dests, desktop_src, service_dests, service_src =  _get_dests_and_srcs(home_path, system)
+    for src, dests in [(desktop_src, desktop_dests), (service_src, service_dests)]:
+        for dest in dests:
+            if isfile(dest):
+                if force or islink(dest):
+                    if force or readlink(dest) == src:
+                        unlink(dest)
+                        print(f'Deleted: {dest}', file=stderr)
+                    else:
+                        real_src = readlink(dest)
+                        print(f'Unexpected symlink: {dest}\n'
+                              f'                 -> {real_src}'
+                              '(use `--force` to delete it anyway)', file=stderr)
+                else:
+                    print(f'Unexpected file: {dest}\n'
+                          '(use `--force` to delete it anyway)', file=stderr)
