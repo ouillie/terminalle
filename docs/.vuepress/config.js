7e6ead0a4eef6edba25e6f2edbe317650b92682e
Will <author@will.party>
2022-05-27 19:02:34 -0700

    Reorganize README

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,20 +8,4 @@ module.exports = {
     repo: 'https://github.com/wm-noble/terminalle',
     docsDir: 'docs',
   }),
-
-  plugins: [
-    {
-      name: 'clean-urls',
-      extendsPage: (page) => {
-        if (!page.frontmatter?.permalink) {
-          const { path } = page
-          if (path.endsWith('.html')) {
-            page.path = path.slice(0, -5)
-          } else if (path.endsWith('/')) {
-            page.path = path.slice(0, -1)
-          }
-        }
-      },
-    },
-  ],
 }
